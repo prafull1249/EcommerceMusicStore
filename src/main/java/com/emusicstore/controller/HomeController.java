c81dcb501a7cdbc04a9fb63055e60bbe09fe6c5c
Prafull Gaikwad <praf1249@gmai.com>
2017-01-22 15:29:44 -0700

    Add footer and header to jsp pages and add product list to the web application. partial commit

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.emusicstore.controller;
 
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by Prafull on 1/22/2017.
@@ -10,9 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    ProductDao productDao = new ProductDao();
+
     @RequestMapping("/")
     public String home(){
         return "home";
+    }
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model){
+
+        List<Product> products = productDao.getProductList();
+        model.addAttribute("products",products );
+        return "productList";
+
     }
 
 }
